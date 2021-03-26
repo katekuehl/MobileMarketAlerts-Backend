0ba74d870854b3e2cf0a3b26f1d6a20acb1a313d
Maeling <maelingmurphy@users.noreply.github.com>
2021-03-26 08:36:45 -0400

    Add Providers class. Update field attributes in Users class

--- main.py
+++ main.py
@@ -20,11 +20,22 @@ db = SQLAlchemy(app)
 class Users(db.Model):
     __tablename__ = 'users'
     id = db.Column('id', db.Integer, primary_key=True)
-    cellphone_number = db.Column('cellphone_number', db.String(120), nullable=False)
-    zip_code = db.Column('zip_code', db.Integer)
+    cellphone_number = db.Column('cellphone_number', db.String(120), nullable=False, unique=True)
+    zip_code = db.Column('zip_code', db.Integer, nullable=False)
 
     def __repr__(self):
         return '<User {}>'.format(self.cellphone_number)
+
+class Providers(db.Model):
+    __tablename__ = 'providers'
+    id = db.Column('id', db.Integer, primary_key=True)
+    name = db.Column('name', db.String(120))
+    address = db.Column('address', db.String(255), nullable=False)
+    description = db.Column('description', db.String(255))
+    website = db.Column('website', db.String(255))
+
+    def __repr__(self):
+        return '<Provider{}>'.format(self.name)
 
 # Routes
 @app.route('/', methods=['GET'])
