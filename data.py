8f84852445d0df5f83e29d2ad92e57879cafda73
Maeling <maelingmurphy@users.noreply.github.com>
2021-03-26 09:59:17 -0400

    Add mock user data in dict

--- data.py
+++ data.py
@@ -1,5 +1,26 @@
 test = "Hello there!"
 
-providers_data = {
+# Data to be imported into database. Structured as list of dicts.
+mock_users = [
+    {
+        'id': 1,
+        'cellphone_number': '111-111-1111',
+        'zip_code': 55411
+    },
+    {
+        'id': 2,
+        'cellphone_number': '222-222-2222',
+        'zip_code': 55411
+    },
+    {
+        'id': 3,
+        'cellphone_number': '333-333-3333',
+        'zip_code': 55411
+    }
+]
 
-}
+providers_data = []
+
+service_types_data = []
+
+events_data = []
